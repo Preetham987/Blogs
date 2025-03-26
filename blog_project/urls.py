a32bc3e5873d8fd62a63e674b7a098de25979f9f
Preetham K R <krpreetham2001@gmail.com>
2025-03-26 19:29:03 +0530

    Initial commit

---
+++ blog_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),  # Add login
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),  # Add logout
+    path('', include('blog.urls')),
+]
